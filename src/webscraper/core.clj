aa307647f6202e18021f51023fb05a9d4815c7b4
shriman <shrimansoft@gmail.com>
2023-09-13 16:30:44 +0530

    news scraping

--- src/webscraper/core.clj
+++ src/webscraper/core.clj
@@ -1,40 +1,75 @@
 (ns webscraper.core
   (:gen-class)
-  (:require [reaver :as rv])
-  )
+  (:require [reaver :as rv]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]))
 
-(def hacker-news (slurp "https://news.ycombinator.com/"))
+(def hacker-news (slurp "https://economictimes.indiatimes.com/archivelist/starttime-44959.cms"))
 
-;; Extract the headlines and urls from the HTML into a seq of maps.
-
-
-;; Example 0
-;; (rv/extract (rv/parse hacker-news) [] "" rv/edn)
-
-;; Example 1
-;; (rv/extract (rv/parse hacker-news) [] ".athing  .title > span > a" rv/edn)
-
-;; Example 2
-;; (rv/extract (rv/parse hacker-news) [] ".titleline " (rv/attr :href))
-
+(defn extract-news-details [news]
+  (let [news-url (str "https://economictimes.indiatimes.com" (:url news))
+        headline (:headline news)
+        news-page (slurp news-url)
+        extracted-data (-> news-page
+                           (rv/parse)
+                           (rv/extract [:author :date :news] ".auth" rv/text ".jsdtTime" rv/text ".artText" rv/text))]
+    (assoc extracted-data :url news-url :heading headline)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println
+  (let [day-news-list (-> hacker-news
+                          (rv/parse)
+                          (rv/extract-from ".content > li" [:headline :url] "a" rv/text "a" (rv/attr :href))
+                          (take 10)
+                          (map extract-news-details))
+        csv-data (->> day-news-list
+                      (map (fn [news]
+                             {:url (:url news)
+                              :heading (:heading news)
+                              :author (:author news)
+                              :date (:date news)
+                              :news (:news news)}))
+                      (cons {:url "URL" :heading "Heading" :author "Author" :date "Date" :news "News"})
+                      (csv/write-csv {:quote? true}))]
+    (io/writer "news_data.csv" csv-data)))
 
-   (rv/extract-from (rv/parse hacker-news) ".itemlist .athing"
-                    [:headline :url]
-                    ".title > a" rv/text
-                    ".title > a" (rv/attr :href))
+;; (ns webscraper.core
 
-   )
+;;   (:gen-class)
+;;   (:require [reaver :as rv])
+;;   )
 
-  (println (rv/parse hacker-news))
+;; (def hacker-news (slurp "https://economictimes.indiatimes.com/archivelist/starttime-44959.cms"))
 
-  (rv/parse hacker-news)
+;; (defn get_news [news]
 
+;;   (let
+;;       [
+;;        news_url   (str "https://economictimes.indiatimes.com" (:url news))
+;;        head_line   (:headline news)
+;;        ]
+;;     (assoc (rv/extract (rv/parse (slurp news_url) ) [:author :date :news] ".auth" rv/text ".jsdtTime" rv/text ".artText" rv/text) :url news_url :heading head_line)
+;;     )
 
+;;   )
 
+;; (defn -main
+;;   "I don't do a whole lot ... yet."
+;;   [& args]
+;;   (let
+;;       [day_news_list (rv/extract-from (rv/parse hacker-news) ".content > li"
+;;                                       [:headline :url]
+;;                                       "a" rv/text
+;;                                       "a" (rv/attr :href))
+;;  ]
+;;     ;; (map get_news day_news_list)
+;;     (map get_news (take 10 day_news_list))
+;;      ;; (rv/extract (rv/parse first_news) [] ".urls") 
+;;     ;; (rv/extract (rv/parse first_news) [] ) 
+;;   )
 
-  )
+;;   (println (rv/parse hacker-news))
+
+;;   (rv/parse hacker-news)
+;;   )
